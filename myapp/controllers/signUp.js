d5ee615caad8b64fc4903ae8f257705d7709297f
idvipul <vipulkaranjkar94@gmail.com>
2017-12-18 21:39:19 +0000

    password encrypted + nav bar added by Front End Team

--- myapp/controllers/signUp.js
+++ myapp/controllers/signUp.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../models/authentication');
 var db = require('../helpers/db')
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -9,13 +11,32 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res) {
-        auth.postUsers(req.body.firstName, req.body.lastName, req.body.email, req.body.password, req.body.accountType, function(err, data){
+
+//	var firstName = req.body.firstNname;
+//  	var lastName = req.body.lastame;
+//	var email = req.body.email;
+//	var password = req.body.password;
+//	var confirmPassword = req.body.confirmPassword;
+//	var accountType = req.body.accountType;
+
+	// Validation
+//	req.checkBody('firstName', 'Name is required').notEmpty();
+//        req.checkBody('lastName', 'Username is required').notEmpty();
+//	req.checkBody('email', 'Email is required').notEmpty();
+//	req.checkBody('email', 'Email is not valid').isEmail();
+//	req.checkBody('password', 'Password is required').notEmpty();
+//	req.checkBody('confirmPassword', 'Passwords do not match').equals(req.body.password);
+//	req.checkBody('accountType', 'Account Type is required').notEmpty();
+
+	bcrypt.hash(req.body.password, saltRounds, function (err, hash) { 
+        auth.postUsers(req.body.firstName, req.body.lastName, req.body.email, hash, req.body.accountType, function(err, data){
         if (err) {
             data = [];
         }
         res.json(req.body)
 //        res.redirect("/fa17g02/")
         });
+     });
   });
 
 module.exports = router;
