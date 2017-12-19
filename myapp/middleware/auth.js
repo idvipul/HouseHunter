7704a338fe329961a0336e0393cd9487410e24dc
Savan Patel <savan.p.755@gmail.com>
2017-12-19 15:05:47 -0800

    Header bug fix

--- myapp/middleware/auth.js
+++ myapp/middleware/auth.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
 	session = req.session
 	if (!req.session.uid) {
 	// res.render('sell');
-		res.redirect('/fa17g02/login');
+		res.redirect('/fa17g02/login', {login: true});
 	}
 	else {
 		next();
